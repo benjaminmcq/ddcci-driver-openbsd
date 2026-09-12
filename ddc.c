bf690b96fa4c39cb7ebc27f8fb9d206d427e63f0
Benjamin Lee McQueen <mcq@disroot.org>
2026-09-11 22:15:04 -0500

    Set up branch for eventual change
    changes were made for wsdisplay integration, and general cleanup.

--- ddc.c
+++ ddc.c
@@ -25,7 +25,6 @@
 
 #include <dev/i2c/i2cvar.h>
 #include <dev/i2c/ddcvar.h>
-#include <dev/i2c/ddc_ioctl.h>
 
 #ifdef DDC_DEBUG
 #define	DPRINTF(x...)		printf(x)
@@ -34,30 +33,21 @@
 #endif
 
 struct ddc_mapping {
-	int			 dm_index;
-	TAILQ_ENTRY(ddc_mapping) dm_link;
 	struct device		*dm_dev;		/* Parent GPU driver instance (referred to as the GPU) */
 	struct i2c_adapter	*dm_adapter;	/* I2C device that initiates transfers from within the GPU (I2C master) */
 	i2c_addr_t		 dm_addr;		/* Target monitor slave (I2C slave) */
 };
-
-static int ddc_next_index = 0;
 
 TAILQ_HEAD(, ddc_mapping) ddcs = TAILQ_HEAD_INITIALIZER(ddcs);
 struct rwlock ddcs_lock = RWLOCK_INITIALIZER("ddclk");
 
 int		ddc_register(struct device *, struct i2c_adapter *, i2c_addr_t);
 void		ddc_unregister(struct device *);
-int		ddc_probe_device(struct device *, struct i2c_adapter *);
 int		ddc_get_vcp(struct device *, struct i2c_adapter *, uint8_t,
     uint16_t *);
 int		ddc_set_vcp(struct device *, struct i2c_adapter *, uint8_t,
     uint16_t);
 unsigned char	ddc_checksum(uint8_t *, unsigned int, i2c_addr_t);
-void		ddcattach(int);
-int		ddcclose(dev_t, int, int, struct proc *);
-int		ddcioctl(dev_t, u_long, caddr_t, int, struct proc *);
-int		ddcopen(dev_t, int, int, struct proc *);
 
 /*
  * Allocate a ddc_mapping for the given GPU driver instance,
@@ -78,7 +68,6 @@ ddc_register(struct device *dev, struct i2c_adapter *adapter, i2c_addr_t addr)
 	dm->dm_addr = addr;
 
 	rw_enter_write(&ddcs_lock);
-	dm->dm_index = ddc_next_index++;
 	TAILQ_INSERT_TAIL(&ddcs, dm, dm_link);
 	rw_exit_write(&ddcs_lock);
 
@@ -106,102 +95,6 @@ ddc_unregister(struct device *dev)
 }
 
 /*
- * Search the registered list of devices for a ddc_mapping whose device
- * field matches the given device.
- * Returns the matching ddc_mapping or NULL if none is found.
- *
- * Send a DDC/CI capabilities request to the device described by the given
- * ddc_mapping, and validate the response utilizing a checksum.
- * Return zero on success, or a negative error code on failure.
- */
-int
-ddc_probe_device(struct device *dev, struct i2c_adapter *adapter)
-{
-	struct ddc_mapping *dm;
-	struct i2c_msg msg;
-	uint8_t data[32], cmd[6];
-	int len, ret;
-	rw_enter_read(&ddcs_lock);
-
-	TAILQ_FOREACH(dm, &ddcs, dm_link) {
-		if (dm->dm_dev == dev && dm->dm_adapter == adapter)
-			break;
-	}
-
-	if (dm == NULL || dm->dm_dev == NULL || dm->dm_adapter == NULL ||
-	    dm->dm_addr == 0) {
-		DPRINTF(
-		    "ddc_probe_device: one or more of the members in ddc_mapping was NULL!");
-		ret = ENOENT;
-		goto out;
-	}
-
-	len = sizeof(cmd) - 1;
-
-	cmd[0] = DDC_HOST_ADDR_ODD;
-	cmd[1] = DDC_PFLAG | 3;
-	cmd[2] = DDC_CMD_CAPS;
-	cmd[3] = 0;
-	cmd[4] = 0;
-	cmd[5] = ddc_checksum(cmd, len, DDC_MONITOR_ADDR << 1);
-
-	msg.addr = dm->dm_addr;
-	msg.flags = 0;
-	msg.buf = cmd;
-	msg.len = sizeof(cmd);
-
-	ret = i2c_transfer(dm->dm_adapter, &msg, 1);
-
-	if (ret < 0) {
-		DPRINTF("ddc_probe_device: write failed! return value=%d\n",
-		    ret);
-		goto out;
-	}
-
-	tsleep_nsec(dm, PWAIT, "ddc", MSEC_TO_NSEC(60));
-
-	msg.flags = I2C_M_RD;
-	msg.buf = data;
-	msg.len = sizeof(data);
-
-	ret = i2c_transfer(dm->dm_adapter, &msg, 1);
-
-	if (ret < 0) {
-		DPRINTF("ddc_probe_device: read failed! return value=%d\n",
-		    ret);
-		goto out;
-	}
-
-	if (data[0] != DDC_DEFAULT_DEVICE_ADDR) {
-		DPRINTF("ddc_probe_device: invalid response! data[0]=0x%x\n",
-		    data[0]);
-		ret = EIO;
-		goto out;
-	}
-
-	/* Strip PFLAG add header and checksum byte to get message length */
-	len = 2 + (data[1] & 0x7F) + 1;
-
-	if (len > sizeof(data)) {
-		DPRINTF("ddc_probe_device: response too long! len=%d\n", len);
-		ret = EIO;
-		goto out;
-	}
-
-	if (ddc_checksum(data, len, DDC_HOST_ADDR_EVEN) != 0) {
-		DPRINTF("ddc_probe_device: checksum failed!\n");
-		ret = EIO;
-		goto out;
-	}
-
-	ret = 0;
-out:
-	rw_exit_read(&ddcs_lock);
-
-	return ret;
-}
-
-/*
  * Send a VCP GET feature request.  On success, write the monitor's reported
  * value into *value.
  */
@@ -224,7 +117,7 @@ ddc_get_vcp(struct device *dev, struct i2c_adapter *adapter, uint8_t vcp_code,
 	if (dm == NULL || dm->dm_dev == NULL || dm->dm_adapter == NULL ||
 	    dm->dm_addr == 0) {
 		DPRINTF(
-		    "ddc_get_vcp: one or more of the members in ddc_mapping was NULL!");
+		    "ddc_get_vcp: one or more of the members in ddc_mapping was NULL!\n");
 		ret = ENOENT;
 		goto out;
 	}
@@ -340,7 +233,7 @@ ddc_set_vcp(struct device *dev, struct i2c_adapter *adapter, uint8_t vcp_code,
 	if (dm == NULL || dm->dm_dev == NULL || dm->dm_adapter == NULL ||
 	    dm->dm_addr == 0) {
 		DPRINTF(
-		    "ddc_set_vcp: one or more of the members in ddc_mapping was NULL!");
+		    "ddc_set_vcp: one or more of the members in ddc_mapping was NULL!\n");
 		ret = ENOENT;
 		goto out;
 	}
@@ -390,102 +283,4 @@ ddc_checksum(uint8_t *cmd, unsigned int len, i2c_addr_t addr)
 		sum ^= cmd[i];
 
 	return (uint8_t)sum;
-}
-
-void
-ddcattach(int nunits)
-{
-}
-
-int
-ddcopen(dev_t dev, int flags, int mode, struct proc *p)
-{
-	struct ddc_mapping *dm;
-
-	rw_enter_write(&ddcs_lock);
-
-	TAILQ_FOREACH(dm, &ddcs, dm_link) {
-		if (dm->dm_index == minor(dev))
-			break;
-	}
-
-	if (dm == NULL) {
-		rw_exit_write(&ddcs_lock);
-		return ENXIO;
-	}
-
-	rw_exit_write(&ddcs_lock);
-
-	return 0;
-}
-
-int
-ddcclose(dev_t dev, int flags, int mode, struct proc *p)
-{
-	return 0;
-}
-
-int
-ddcioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
-{
-	struct ddc_vcp_args *dva = (struct ddc_vcp_args *)data;
-	struct ddc_mapping *dm;
-
-	switch (cmd) {
-	case DDCIOCPROBE:
-		rw_enter_read(&ddcs_lock);
-
-		dm = NULL;
-
-		TAILQ_FOREACH(dm, &ddcs, dm_link) {
-			if (minor(dev) == dm->dm_index)
-				break;
-		}
-
-		rw_exit_read(&ddcs_lock);
-
-		if (dm == NULL)
-			return ENOENT;
-
-		return ddc_probe_device(dm->dm_dev, dm->dm_adapter);
-
-	case DDCIOCGETBRIGHTNESS:
-		rw_enter_read(&ddcs_lock);
-
-		dm = NULL;
-
-		TAILQ_FOREACH(dm, &ddcs, dm_link) {
-			if (minor(dev) == dm->dm_index)
-				break;
-		}
-
-		rw_exit_read(&ddcs_lock);
-
-		if (dm == NULL)
-			return ENOENT;
-
-		return ddc_get_vcp(dm->dm_dev, dm->dm_adapter, 0x10,
-		    &dva->dva_value);
-
-	case DDCIOCSETBRIGHTNESS:
-		rw_enter_read(&ddcs_lock);
-
-		dm = NULL;
-
-		TAILQ_FOREACH(dm, &ddcs, dm_link) {
-			if (minor(dev) == dm->dm_index)
-				break;
-		}
-
-		rw_exit_read(&ddcs_lock);
-
-		if (dm == NULL)
-			return ENOENT;
-
-		return ddc_set_vcp(dm->dm_dev, dm->dm_adapter, 0x10,
-		    dva->dva_value);
-
-	default:
-		return ENOTTY;
-	}
 }
